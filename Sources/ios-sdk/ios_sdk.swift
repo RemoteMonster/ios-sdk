86b57e8bd6046355bfc9a1c54ab42fd84ef2d586
dididi <dfdgsdfg@gmail.com>
2018-05-25 19:05:11 +0900

    chore: add SwiftPM

---
+++ Sources/ios-sdk/ios_sdk.swift
@@ -0,0 +1,3 @@
+struct ios_sdk {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import ios_sdkTests
+
+var tests = [XCTestCaseEntry]()
+tests += ios_sdkTests.allTests()
+XCTMain(tests)
