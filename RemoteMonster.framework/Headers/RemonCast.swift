7afe5d2d38ece735e656f62cba0d6230c01d311e
hyounsiklee <hyounsik@remotemonster.com>
2018-05-28 14:00:58 +0900

    2.0.5

---
+++ RemoteMonster.framework/Headers/RemonCast.swift
@@ -0,0 +1,47 @@
+//
+//  RemonCast.swift
+//  remonios
+//
+//  Created by hyounsiklee on 2018. 5. 10..
+//  Copyright © 2018년 Remote Monster. All rights reserved.
+//
+
+import UIKit
+
+public class RemonCast: RemonIBController, RemonControllBlockSettable {
+    private var broardcast:Bool {
+        get {
+            if self.channelType_ == 2 {
+                return true
+            } else {
+                return false
+            }
+        }
+        set(broardcast) {
+            if broardcast {
+                self.channelType = 2
+            } else {
+                self.channelType = 1
+            }
+        }
+    }
+    
+    override public init() {
+        super.init()
+        self.broardcast = true
+    }
+    
+    override public func joinRoom(chID: String, _ config:RemonConfig? = nil) {
+        self.broardcast = false
+        super.joinRoom(chID: chID, config)
+    }
+    
+    override public func createRoom(_ config:RemonConfig? = nil) {
+        self.broardcast = true
+        super.createRoom(config)
+    }
+    
+    public func search(complete: @escaping (RemonError?, Array<RemonSearchResult>?) -> Void) {
+        super.search(type: .cast, complete: complete)
+    }
+}
