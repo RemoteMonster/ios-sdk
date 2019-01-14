20cdf3296fbd8005a03e7af5d280c936997fd802
leehyounsik <hyounsik@remotemonster.com>
2019-01-14 18:27:13 +0900

    ex mesh

--- examples/full/meshCall/MeshCallViewController.swift
+++ examples/full/meshCall/MeshCallViewController.swift
@@ -26,54 +26,124 @@ class MeshCallViewController: UIViewController {
     @IBOutlet weak var chField_5: UITextField!
     
     
+    @IBOutlet weak var chLabel_0: UILabel!
+    @IBOutlet weak var chLabel_1: UILabel!
+    @IBOutlet weak var chLabel_2: UILabel!
+    @IBOutlet weak var chLabel_3: UILabel!
+    @IBOutlet weak var chLabel_4: UILabel!
+    @IBOutlet weak var chLabel_5: UILabel!
+    
+    @IBOutlet weak var chButton_0: UIButton!
+    @IBOutlet weak var chButton_1: UIButton!
+    @IBOutlet weak var chButton_2: UIButton!
+    @IBOutlet weak var chButton_3: UIButton!
+    @IBOutlet weak var chButton_4: UIButton!
+    @IBOutlet weak var chButton_5: UIButton!
+    
+    @IBAction func endCh_0(_ sender: UIControl) {
+        self.chButton_0.isEnabled = true
+        self.disconnectChannel(label: self.chLabel_0, remon: remonCall_0)
+    }
+    
+    @IBAction func endCh_1(_ sender: UIControl) {
+        self.chButton_1.isEnabled = true
+        self.disconnectChannel(label: self.chLabel_1, remon: remonCall_1)
+    }
+    
+    @IBAction func endCh_2(_ sender: UIControl) {
+        self.chButton_2.isEnabled = true
+        self.disconnectChannel(label: self.chLabel_2, remon: remonCall_2)
+    }
+    
+    @IBAction func endCh_3(_ sender: UIControl) {
+        self.chButton_3.isEnabled = true
+        self.disconnectChannel(label: self.chLabel_3, remon: remonCall_3)
+    }
+    
+    @IBAction func endCh_4(_ sender: UIControl) {
+        self.chButton_4.isEnabled = true
+        self.disconnectChannel(label: self.chLabel_4, remon: remonCall_4)
+    }
+    
+    @IBAction func endCh_5(_ sender: UIControl) {
+        self.chButton_5.isEnabled = true
+        self.disconnectChannel(label: self.chLabel_5, remon: remonCall_5)
+    }
+    
+    
     @IBAction func goCh_0(_ sender: UIControl) {
+        sender.isEnabled = false
         if let chid = self.chField_0.text {
-            self.connectChannel(chid: chid, remon: remonCall_0)
+            self.connectChannel(chid: chid, label:self.chLabel_0, remon: remonCall_0)
         }
     }
     
     @IBAction func goCh_1(_ sender: UIControl) {
+        sender.isEnabled = false
         if let chid = self.chField_1.text {
-            self.connectChannel(chid: chid, remon: remonCall_1)
+            self.connectChannel(chid: chid, label:self.chLabel_1, remon: remonCall_1)
         }
     }
     
     @IBAction func goCh_2(_ sender: UIControl) {
+        sender.isEnabled = false
         if let chid = self.chField_2.text {
-            self.connectChannel(chid: chid, remon: remonCall_2)
+            self.connectChannel(chid: chid, label:self.chLabel_2, remon: remonCall_2)
         }
     }
     
     @IBAction func goCh_3(_ sender: UIControl) {
+        sender.isEnabled = false
         if let chid = self.chField_3.text {
-            self.connectChannel(chid: chid, remon: remonCall_3)
+            self.connectChannel(chid: chid, label:self.chLabel_3, remon: remonCall_3)
         }
     }
     
     @IBAction func goCh_4(_ sender: UIControl) {
+        sender.isEnabled = false
         if let chid = self.chField_4.text {
-            self.connectChannel(chid: chid, remon: remonCall_4)
+            self.connectChannel(chid: chid, label:self.chLabel_4, remon: remonCall_4)
         }
     }
     
     @IBAction func goCh_5(_ sender: UIControl) {
+        sender.isEnabled = false
         if let chid = self.chField_5.text {
-            self.connectChannel(chid: chid, remon: remonCall_5)
+            self.connectChannel(chid: chid, label:self.chLabel_5, remon: remonCall_5)
         }
     }
     
-    func connectChannel(chid:String, remon:RemonCall) -> Void {
+    func connectChannel(chid:String, label:UILabel, remon:RemonCall) -> Void {
         remon.connect(chid)
         
+        self.hideKeyboard()
+        
         remon.onConnect { (chid) in
-            print("mesh connect", chid)
+            DispatchQueue.main.async {
+                label.text = chid
+            }
+            if let chid = chid {
+                print("mesh connect", chid)
+            } else {
+                print("mesh connect", "dmd?")
+            }
         }
         
         remon.onComplete {
             print("mesh complete")
-            
-            
         }
+        
+        remon.onClose { (type) in
+            print("mesh close", type)
+            
+            DispatchQueue.main.async {
+                label.text = ""
+            }
+        }
+    }
+    
+    func disconnectChannel(label:UILabel, remon:RemonCall) -> Void {
+        remon.closeRemon()
     }
     
     override func viewDidLoad() {
@@ -81,6 +151,8 @@ class MeshCallViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    func hideKeyboard() -> Void {
+        self.view.endEditing(true)
+    }
 }
 
