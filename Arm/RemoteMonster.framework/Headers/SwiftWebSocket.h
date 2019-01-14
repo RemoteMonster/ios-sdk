fbb9759af685d765e754b34a5e01e9bbb1b69d1c
leehyounsik <hyounsik@remotemonster.com>
2019-01-14 13:58:53 +0900

    arm 만 지원 하는 프래임워크 추가.

---
+++ Arm/RemoteMonster.framework/Headers/SwiftWebSocket.h
@@ -0,0 +1,16 @@
+//
+//  SwiftWebSocket.h
+//  minisignal
+//
+//  Created by 최진호 on 2016. 9. 29..
+//  Copyright © 2016년 최진호. All rights reserved.
+//
+
+#ifndef SwiftWebSocket_h
+#define SwiftWebSocket_h
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double SwiftWebSocketVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftWebSocketVersionString[];
+
+#endif /* SwiftWebSocket_h */
