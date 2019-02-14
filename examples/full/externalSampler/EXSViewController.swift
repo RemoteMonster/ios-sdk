21c803bd922d38e0e641c1a75f196341201982d1
leehyounsik <hyounsik@remotemonster.com>
2019-02-14 12:15:03 +0900

    feat: - externalSampler 오류 수정 - outputImageOrientation 수정 - 동작 테스트

--- examples/full/externalSampler/EXSViewController.swift
+++ examples/full/externalSampler/EXSViewController.swift
@@ -14,19 +14,28 @@ class EXSViewController: UIViewController, GPUImageVideoCameraDelegate {
     @IBOutlet var remonCall: RemonCall!
     @IBOutlet weak var chField: UITextField!
     @IBOutlet weak var enBtn: UIButton!
+    @IBOutlet weak var localView: UIView!
+    @IBOutlet weak var rotateButton: UIButton!
     
     var videoCamera:GPUImageVideoCamera?
     private var cmTime:CMTime?
+    private var NOOP:Bool = false
+    private var mySampleBuffer:CVPixelBuffer?
     
     @IBAction func enterChannel(_ sender: UIButton) {
         if let chid = self.chField.text {
             self.remonCall.connect(chid)
         }
+        self.view.endEditing(true)
+    }
+    
+    
+    @IBAction func exitChannel(_ sender: UIButton) {
+        self.remonCall.closeRemon()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        외부 샘플러를 이용하겠다고 선언 합니다.
         self.remonCall.useExternalCapturer = true
         
         self.remonCall.onComplete {
@@ -36,6 +45,14 @@ class EXSViewController: UIViewController, GPUImageVideoCameraDelegate {
     
     func willOutputSampleBuffer(_ sampleBuffer: CMSampleBuffer!) {
         self.cmTime = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
+        
+        if (CMSampleBufferGetNumSamples(sampleBuffer) != 1 || !CMSampleBufferIsValid(sampleBuffer) ||
+            !CMSampleBufferDataIsReady(sampleBuffer)) {
+            self.NOOP = true
+        } else {
+            self.NOOP = false
+            self.mySampleBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
+        }
     }
     
     private func startCapture() -> Void {
@@ -43,17 +60,31 @@ class EXSViewController: UIViewController, GPUImageVideoCameraDelegate {
         self.videoCamera = GPUImageVideoCamera.init(sessionPreset: AVCaptureSession.Preset.vga640x480.rawValue, cameraPosition: gpuImageSource)
         
         let filter:GPUImageGaussianBlurFilter = GPUImageGaussianBlurFilter()
-        filter.blurRadiusInPixels = 10.0
+        
+        
+        filter.blurRadiusInPixels = 5.0
         if let videoCamera = self.videoCamera {
             videoCamera.delegate = self
             videoCamera.addTarget(filter)
             videoCamera.frameRate = 24
+            videoCamera.outputImageOrientation = .portrait
+            /**
+             ExternalCapturer를 사용 할 경우 Remon이 제공 하는 preview를 사용 할 수 없게 됩니다.
+             이 예제에서는 GPUImageView를 이용 하여 자신의 영상을 구현 하고 있습니다.
+             **/
+            DispatchQueue.main.async {
+                let viewFrame = CGRect(origin: CGPoint.zero, size: self.localView.frame.size)
+                let filteredVideoView:GPUImageView = GPUImageView.init(frame: viewFrame)
+                self.localView.addSubview(filteredVideoView)
+                filter.addTarget(filteredVideoView)
+            }
             
             if let op = GPUImageRawDataOutput.init(imageSize: CGSize.init(width: 480, height: 640), resultsInBGRAFormat: true) {
                 filter.addTarget(op)
                 videoCamera.startCapture()
                 
                 op.newFrameAvailableBlock = { () in
+                    if self.NOOP { return }
                     
                     let width = Int(op.maximumOutputSize().width)
                     let height = Int(op.maximumOutputSize().height)
@@ -71,8 +102,7 @@ class EXSViewController: UIViewController, GPUImageVideoCameraDelegate {
                                                               width*4,
                                                               { releaseContext, baseAddress in
                                                                 let contextData = Unmanaged<CFData>.fromOpaque(releaseContext!)
-                                                                contextData.release()
-                    },
+                                                                contextData.release()},
                                                               unmanagedData.toOpaque(),
                                                               nil,
                                                               &pixelBuffer)
@@ -83,17 +113,21 @@ class EXSViewController: UIViewController, GPUImageVideoCameraDelegate {
                     
                     if let pixelBuffer = pixelBuffer {
                         if let cmTime = self.cmTime {
-//                            remonCall의 외부샘플러델리게이터에게 프래임을 넘깁니다.
+                            
+                            /**
+                             이 예제에서는 통신 연결이 완료 된후 startCapture()이 호출 됩니다.
+                             만약 연결이 완료 되지 않은 상태에서 startCapture()가 호출 되지 않았다면 아래 분기는 실행 되지 않습니다.
+                             이후 연결이 완료되어 localExternalCaptureDelegator가 획득 된다면 아래 분기는 실행 됩니다.
+                             **/
                             if let rtcCaptureDelegate = self.remonCall.localExternalCaptureDelegator {
-                                rtcCaptureDelegate.didCaptureFrame(pixelBuffer: pixelBuffer, timeStampNs: cmTime.value, videoRetation: ._0)
+                                rtcCaptureDelegate.didCaptureFrame(pixelBuffer: pixelBuffer, timeStamp: cmTime, videoRetation: ._0)
                             }
                         }
                     }
                 }
+                
             }
         }
     }
-    
-    
+                
 }
-
