425387759b15712547935ad4e9adcf4e3033c508
Chance <liveupdate.all@gmail.com>
2019-06-19 20:25:45 +0900

    chore(release):v2.4.496

--- RemoteMonster.framework/Headers/SwiftWebSocket.h
+++ RemoteMonster.framework/Headers/SwiftWebSocket.h
@@ -1,16 +1,15 @@
-//
-//  SwiftWebSocket.h
-//  minisignal
-//
-//  Created by 최진호 on 2016. 9. 29..
-//  Copyright © 2016년 최진호. All rights reserved.
-//
+/*
+ * SwiftWebSocket (swiftwebsocket.h)
+ *
+ * Copyright (C) Josh Baker. All Rights Reserved.
+ * Contact: @tidwall, joshbaker77@gmail.com
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ */
 
-#ifndef SwiftWebSocket_h
-#define SwiftWebSocket_h
 #import <Foundation/Foundation.h>
 
 FOUNDATION_EXPORT double SwiftWebSocketVersionNumber;
 FOUNDATION_EXPORT const unsigned char SwiftWebSocketVersionString[];
-
-#endif /* SwiftWebSocket_h */
