138f418294f6c6a37a5cf1e7ae7536b18215cf7d
Chance <chance@remotemonster.com>
2020-05-06 14:18:09 +0900

    chore(release): RemoteMonster SDK v2.7.3

---
+++ RemoteMonster.xcframework/ios-i386_x86_64-simulator/RemoteMonster.framework/Headers/RemoteMonster.h
@@ -0,0 +1,20 @@
+//
+//  remoniosv2.h
+//  remoniosv2
+//
+//  Created by 최진호 on 2017. 1. 31..
+//  Copyright © 2017년 Remote Monster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for remoniosv2.
+FOUNDATION_EXPORT double remoniosv2VersionNumber;
+
+//! Project version string for remoniosv2.
+FOUNDATION_EXPORT const unsigned char remoniosv2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <remoniosv2/PublicHeader.h>
+#import "ARDSDPUtils.h"
+#import "SwiftWebSocket.h"
+#import <WebRTC/WebRTC.h>
